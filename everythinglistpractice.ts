c9d5f78bdf83acc64afef9c522763a56cb0f5c11
hector3211 <hectororopesa5@gmail.com>
2023-03-14 19:48:04 -0400

    added: breadthfirstsearch, compare two trees

--- everythinglistpractice.ts
+++ everythinglistpractice.ts
@@ -1,126 +1,126 @@
 type ANode<T> = {
-  next: ANode<T> | undefined;
-  prev: ANode<T> | undefined;
-  value: T;
+    next: ANode<T> | undefined;
+    prev: ANode<T> | undefined;
+    value: T;
 };
 
 class DoubleList<T> {
-  head: ANode<T> | undefined;
-  tail: ANode<T> | undefined;
-  length: number;
+    head: ANode<T> | undefined;
+    tail: ANode<T> | undefined;
+    length: number;
 
-  constructor() {
-    this.head = undefined;
-    this.tail = undefined;
-    this.length = 0;
-  }
-
-  prepend(item: T) {
-    const new_node = { value: item } as ANode<T>;
-    this.length++;
-    if (!this.head) {
-      this.head = this.tail = new_node;
-      return;
-    }
-    new_node.next = this.head;
-    this.head.prev = new_node;
-    this.head = new_node;
-  }
-
-  append(item: T) {
-    const new_node = { value: item } as ANode<T>;
-    this.length++;
-    if (!this.tail) {
-      this.head = this.tail = new_node;
-    }
-    new_node.prev = this.tail;
-    this.tail.next = new_node;
-    this.tail = new_node;
-  }
-
-  insert_at(idx: T, item: T) {
-    const new_node = { value: item } as ANode<T>;
-    if (!this.head || idx > this.length) {
-      return;
-    }
-    this.length++;
-
-    if (idx === this.length) {
-      this.append(item);
-      return;
-    }
-    if (idx === 0) {
-      this.prepend(item);
-      return;
+    constructor() {
+        this.head = undefined;
+        this.tail = undefined;
+        this.length = 0;
     }
 
-    let current = this.head;
-    for (let i = 0; current && i < idx; i++) {
-      if (current.next) {
-        current = current.next;
-      }
-    }
-    console.log(`Adding -- ${item}`);
-    new_node.next = current.next;
-    new_node.prev = current;
-    current.next = new_node;
-    if (current.prev) {
-      current.prev.next = current;
-    }
-  }
-
-  remove(item: T) {
-    if (!this.head) {
-      return;
+    prepend(item: T) {
+        const new_node = { value: item } as ANode<T>;
+        this.length++;
+        if (!this.head) {
+            this.head = this.tail = new_node;
+            return;
+        }
+        new_node.next = this.head;
+        this.head.prev = new_node;
+        this.head = new_node;
     }
 
-    if (this.head.value === item) {
-      this.head = this.head.next;
-      return;
+    append(item: T) {
+        const new_node = { value: item } as ANode<T>;
+        this.length++;
+        if (!this.tail) {
+            this.head = this.tail = new_node;
+        }
+        new_node.prev = this.tail;
+        this.tail.next = new_node;
+        this.tail = new_node;
     }
 
-    let current = this.head?.next;
-    while (current) {
-      if (current.value === item) {
-        break;
-      }
-      current = current.next;
+    insert_at(idx: T, item: T) {
+        const new_node = { value: item } as ANode<T>;
+        if (!this.head || idx > this.length) {
+            return;
+        }
+        this.length++;
+
+        if (idx === this.length) {
+            this.append(item);
+            return;
+        }
+        if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
+
+        let current = this.head;
+        for (let i = 0; current && i < idx; i++) {
+            if (current.next) {
+                current = current.next;
+            }
+        }
+        console.log(`Adding -- ${item}`);
+        new_node.next = current.next;
+        new_node.prev = current;
+        current.next = new_node;
+        if (current.prev) {
+            current.prev.next = current;
+        }
     }
 
-    if (!current) {
-      return;
+    remove(item: T) {
+        if (!this.head) {
+            return;
+        }
+
+        if (this.head.value === item) {
+            this.head = this.head.next;
+            return;
+        }
+
+        let current = this.head?.next;
+        while (current) {
+            if (current.value === item) {
+                break;
+            }
+            current = current.next;
+        }
+
+        if (!current) {
+            return;
+        }
+
+        console.log(`Removing: ${item}`);
+        this.length--;
+
+        if (current.prev) {
+            current.prev.next = current.next;
+        }
+
+        if (current.next) {
+            current.next.prev = current.prev;
+        }
+        if (current === this.tail) {
+            this.tail = current.prev;
+        }
     }
 
-    console.log(`Removing: ${item}`);
-    this.length--;
-
-    if (current.prev) {
-      current.prev.next = current.next;
+    get_all() {
+        if (!this.head) {
+            return;
+        }
+        console.log("Getting nodes...");
+        let current = this.head;
+        while (current) {
+            if (!current.next) {
+                console.log(`value -- ${current.value}`);
+                break;
+            }
+            console.log(`value -- ${current.value}`);
+            current = current.next;
+        }
     }
-
-    if (current.next) {
-      current.next.prev = current.prev;
-    }
-    if (current === this.tail) {
-      this.tail = current.prev;
-    }
-  }
-
-  get_all() {
-    if (!this.head) {
-      return;
-    }
-    console.log("Getting nodes...");
-    let current = this.head;
-    while (current) {
-      if (!current.next) {
-        console.log(`value -- ${current.value}`);
-        break;
-      }
-      console.log(`value -- ${current.value}`);
-      current = current.next;
-    }
-  }
 }
 
 let dlist = new DoubleList();
--- trees.ts
+++ trees.ts
@@ -1,96 +1,165 @@
 type TNode<T> = {
-  left: TNode<T>;
-  right: TNode<T>;
-  value: T;
+    left: TNode<T> | undefined;
+    right: TNode<T> | undefined;
+    value: T;
 };
 
-class Tree<T> {
-  root: TNode<T> | undefined;
+interface TreeActions<T> {
+    append(item: T): void;
+    search(item: T): boolean;
+}
 
-  constructor() {
-    this.root = undefined;
-  }
+class Tree<T> implements TreeActions<T> {
+    root: TNode<T> | undefined;
 
-  append(item: T) {
-    const new_node = { value: item } as TNode<T>;
-    if (!this.root) {
-      this.root = new_node;
-      return;
+    constructor() {
+        this.root = undefined;
     }
 
-    let current = this.root;
-    while (current) {
-      if (item > current.value) {
-        if (!current.right) {
-          current.right = new_node;
-          return;
-        } else {
-          current = current.right;
+    append(item: T) {
+        const new_node = { value: item } as TNode<T>;
+        if (!this.root) {
+            this.root = new_node;
+            return;
         }
-      } else {
-        if (!current.left) {
-          current.left = new_node;
-          return;
-        } else {
-          current = current.left;
-        }
-      }
-    }
-  }
 
-  search(item: T): boolean {
-    let current = this.root;
-    while (current) {
-      if (current.value === item) {
-        return true;
-      } else if (current.value > item) {
-        current = current.right;
-      } else {
-        current = current.left;
-      }
+        let current = this.root;
+        while (current) {
+            if (item > current.value) {
+                if (!current.right) {
+                    current.right = new_node;
+                    return;
+                } else {
+                    current = current.right;
+                }
+            } else {
+                if (!current.left) {
+                    current.left = new_node;
+                    return;
+                } else {
+                    current = current.left;
+                }
+            }
+        }
+    }
+
+    search(item: T): boolean {
+        let current = this.root;
+        while (current) {
+            if (current.value === item) {
+                return true;
+            } else if (current.value > item) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
+        return false;
+    }
+}
+
+function in_order(node: TNode<number> | undefined): void {
+    if (!node) {
+        return;
+    } else {
+        let current = node;
+        in_order(current.left);
+        console.log(current.value);
+        in_order(current.right);
+    }
+}
+
+function pre_order(node: TNode<number> | undefined): void {
+    if (!node) {
+        return;
+    } else {
+        console.log(node.value);
+        pre_order(node.left);
+        pre_order(node.right);
+    }
+}
+
+function post_order(node: TNode<number> | undefined): void {
+    if (!node) {
+        return;
+    } else {
+        post_order(node.left);
+        post_order(node.right);
+        console.log(node.value);
+    }
+}
+
+// This is probably spelled wrong
+function breadthFirstSearch(
+    node: TNode<number> | undefined,
+    needle: number
+): boolean {
+    if (!node) {
+        return false;
+    }
+
+    // creating a queue!
+    const que = [node];
+
+    while (que.length) {
+        // getting first item in queue as TNode of course!
+        const current = que.shift() as TNode<number> | undefined;
+
+        if (current) {
+            // check if we get a match!
+            if (current.value === needle) {
+                return true;
+            }
+            // if no match!
+            // we push in left
+            if (current.left) {
+                que.push(current.left);
+            }
+            // we push in right
+            if (current.right) {
+                que.push(current.right);
+            }
+        }
+
     }
     return false;
-  }
 }
 
-function in_order<T>(node: TNode<T>): void {
-  if (!node) {
-    return;
-  } else {
-    let current = node;
-    in_order(current.left);
-    console.log(current.value);
-    in_order(current.right);
-  }
-}
-
-function pre_order<T>(node: TNode<T>): void {
-  if (!node) {
-    return;
-  } else {
-    console.log(node.value);
-    pre_order(node.left);
-    pre_order(node.right);
-  }
-}
-
-function post_order<T>(node: TNode<T>): void {
-  if (!node) {
-    return;
-  } else {
-    post_order(node.right);
-    console.log(node.value);
-    post_order(node.left);
-  }
+function compareTrees(
+    a: TNode<number> | undefined,
+    b: TNode<number> | undefined
+): boolean {
+    if (a === undefined && b === undefined) {
+        return true;
+    }
+    if (a === undefined || b === undefined) {
+        return false;
+    }
+    if (a.value !== b.value) {
+        return false;
+    }
+    return compareTrees(a.left, b.left) && compareTrees(a.right, b.right);
 }
 
 let tree = new Tree();
 tree.append(10);
 tree.append(5);
 tree.append(12);
-console.log("In order...");
-in_order(tree.root as TNode<number>);
+
+let treeTwo = new Tree();
+treeTwo.append(10);
+treeTwo.append(5);
+treeTwo.append(12);
+treeTwo.append(2);
+
 console.log("Pre order...");
 pre_order(tree.root as TNode<number>);
+console.log("In order...");
+in_order(tree.root as TNode<number>);
 console.log("Post order...");
 post_order(tree.root as TNode<number>);
+
+console.log("Breadth First Search...");
+console.log(breadthFirstSearch(tree.root as TNode<number>, 15));
+// Compare two trees!
+console.log(compareTrees(tree.root as TNode<number>, treeTwo.root as TNode<number>));
